8831f8b70009c62ea05534e06cd79ef9bd9fbd0c
Christopher Cotton <ccotton@opentext.com>
2025-09-16 16:25:26 -0500

    updated die_roll_player_can_roll_2_dice

--- Monopoly.UnitTests/DieTests.cs
+++ Monopoly.UnitTests/DieTests.cs
@@ -19,7 +19,7 @@
                 Assert.Fail();
             }
         }
-
+        
         [TestMethod]
         public void Die_Roll_Player_Can_Roll_2_Dice()
         {
@@ -27,13 +27,22 @@
             //Create game
             //Create player
             //Create 2 dice
+            var d1 = new Die();
+            var d2 = new Die();
 
             //Act
             //Roll dice 1
             //Roll dice 2
+            Die.RollMultiple(d1, d2);
+            int result1 = d1.FaceValue;
+            int result2 = d2.FaceValue;
+            int total = result1 + result2;
 
             //Assert
             //Assert that both dice were rolled and are valid (value = 1 -6, total between 2 and 12)
+            Assert.IsTrue(result1 >= 1 && result1 <= 6, $"Die 1 roll is out of bounds: {result1}");
+            Assert.IsTrue(result2 >= 1 && result2 <= 6, $"Die 2 roll is out of bounds: {result2}");
+            Assert.IsTrue(total >= 2 && total <= 12, $"Total roll is out of bounds: {total}");
         }
 
         [TestMethod]
@@ -44,11 +53,11 @@
             Given_BothDice_Are_Same_Value();
             
             // Act
-            PerformTurn();
+            // PerformTurn();
             
             // Assert
             // Should still be player 1's turn
-            // Player 1 should have moved the correct number of spaces
+            // Player 1 should have moved 6 spaces
             // Player 1 should have the ability to roll the dice again
 
         }
@@ -66,6 +75,7 @@
 
         public void Given_BothDice_Are_Same_Value()
         {
+            // Return a dice value of 3 and a dice value of 3
         }
 
 
--- Monopoly/Die.cs
+++ Monopoly/Die.cs
@@ -1,21 +1,56 @@
-﻿
-namespace Monopoly
+﻿namespace Monopoly
 {
   public class Die
   {
-    int _faceValue;
-    public int FaceValue
-    {
-      get
-      {
-        return _faceValue;
-      }
-    }
-    private static Random _random = new Random();
+    // Current face value of the die
+    private int _faceValue;
 
+    // Number of sides on the die
+    private readonly int _sides;
+
+    // Shared random number generator for all dice
+    private static readonly Random _random = new Random();
+
+    // Public property to expose number of sides
+    public int Sides => _sides;
+
+    // Public property to expose current face value
+    public int FaceValue => _faceValue;
+
+    // Default constructor creates a 6-sided die
+    public Die() : this(6) { }
+
+    // Constructor for a die with a custom number of sides
+    public Die(int sides)
+    {
+      if (sides < 2)
+        throw new ArgumentException("A die must have at least 2 sides.");
+      _sides = sides;
+      Roll(); // initialize with a valid value
+    }
+
+    // Roll the die to get a random face value
     public void Roll()
     {
-      _faceValue = _random.Next(1, 6);
+      _faceValue = _random.Next(1, _sides + 1);
+    }
+
+    // Return the face value as a string like "[3]"
+    public override string ToString()
+    {
+      return $"[{_faceValue}]";
+    }
+
+    // Roll multiple dice at once and return the sum
+    public static int RollMultiple(params Die[] dice)
+    {
+      int total = 0;
+      foreach (var die in dice)
+      {
+        die.Roll();
+        total += die.FaceValue;
+      }
+      return total;
     }
   }
 }
