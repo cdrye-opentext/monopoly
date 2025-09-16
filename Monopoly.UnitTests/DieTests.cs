f53b4d8808b36e1617a39382004288e009806846
Christopher Cotton <ccotton@opentext.com>
2025-09-16 14:09:55 -0500

    unit test for double roll.

--- Monopoly.UnitTests/DieTests.cs
+++ Monopoly.UnitTests/DieTests.cs
@@ -14,12 +14,15 @@
             int result = d.FaceValue;
 
             // Assert
-            if (result < 1 || result > 6) { Assert.Fail(); }
+            if (result < 1 || result > 6)
+            {
+                Assert.Fail();
+            }
         }
 
         [TestMethod]
         public void Die_Roll_Player_Can_Roll_2_Dice()
-        { 
+        {
             //Arrange
             //Create game
             //Create player
@@ -33,6 +36,38 @@
             //Assert that both dice were rolled and are valid (value = 1 -6, total between 2 and 12)
         }
 
+        [TestMethod]
+        public void Handle_Double_Die_Roll()
+        {
+            // Arrange
+            Given_SetupGame();
+            Given_BothDice_Are_Same_Value();
+            
+            // Act
+            PerformTurn();
+            
+            // Assert
+            // Should still be player 1's turn
+            // Player 1 should have moved the correct number of spaces
+            // Player 1 should have the ability to roll the dice again
+
+        }
 
 
+        [TestInitialize]
+        public void Given_SetupGame()
+        {
+            //Arrange
+            //Create game
+            //Create player
+            //Create 2 dice
+        }
+
+
+        public void Given_BothDice_Are_Same_Value()
+        {
+        }
+
+
+    }
 }
