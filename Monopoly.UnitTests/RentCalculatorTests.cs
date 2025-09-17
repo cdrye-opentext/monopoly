6112d141619117f483584bee8d039a5dc5afdef9
Christopher Cotton <ccotton@opentext.com>
2025-09-17 15:26:04 -0500

    Seaming and sprouting with boardwalk

--- Monopoly.UnitTests/RentCalculatorTests.cs
+++ Monopoly.UnitTests/RentCalculatorTests.cs
@@ -34,5 +34,42 @@ namespace Monopoly.UnitTests
             Assert.AreEqual(rent, 70);
 
         }
+
+        [TestMethod]
+        public void Given_Boardwalk_Has_No_Houses_And_No_Hotel_And_Isnt_Mortgaged_And_Isnt_Monopolized_Then_Calculate_Rent()
+        {
+            // Arrange & Act
+            var rent = RentCalculator.CalculateRent("Boardwalk", 0,false,false,"Blue");
+            // Assert
+            Assert.AreEqual(rent, 50);
+        }
+        
+        [TestMethod]
+        [DataRow(0,false,"DarkBlue",100)]
+        [DataRow(0,false,"Blue",50)]
+        [DataRow(1,false,"DarkBlue",200)]
+        [DataRow(2,false,"DarkBlue",600)]
+        [DataRow(3,false,"DarkBlue",1100)]
+        [DataRow(4,false,"DarkBlue",1700)]
+        [DataRow(0,true,"DarkBlue",2000)] // with hotel
+        // // mortgaged
+        // [DataRow(0,false,true,"DarkBlue",0)]
+        // [DataRow(1,false,true,"DarkBlue",0)]
+        // [DataRow(2,false,true,"DarkBlue",0)]
+        // [DataRow(3,false,true,"DarkBlue",0)]
+        // [DataRow(4,false,true,"DarkBlue",0)]
+        // [DataRow(0,true,true,"DarkBlue",0)] // with hotel
+        public void Given_Boardwalk_Has_No_Houses_And_No_Hotel_And_Isnt_Mortgaged_And_Isnt_Monopolized_Then_Calculate_Rent(
+            int houseCount, 
+            bool hasHotel, 
+            string color, 
+            int expectedRent
+            )
+        {
+            // Arrange & Act
+            var rent = RentCalculator.RentForBoardwalk(houseCount, hasHotel, color);
+            // Assert
+            Assert.AreEqual(rent, expectedRent);
+        }
     }
 }
--- Monopoly/RentCalculator.cs
+++ Monopoly/RentCalculator.cs
@@ -2,11 +2,125 @@ namespace Monopoly
 {
   public class RentCalculator
   {
-    // Implement GetRent method using TDD
-
-    public int GetRent(Property property)
+    // Warning: legacy mess. Do not touch unless you have band-aids nearby.
+    public static int CalculateRent(string propertyName, int houses, bool hotel, bool isMortgaged, string colorGroupOwner)
     {
-      return property.GetCurrentRent();
+      int rent = 0;
+
+      if (propertyName == "Boardwalk")
+      {
+        rent = RentForBoardwalk(houses, hotel, colorGroupOwner);
+      }
+      else if (propertyName == "Park Place")
+      {
+        if (hotel)
+        {
+          rent = 1500;
+        }
+        else if (houses == 4)
+        {
+          rent = 1100;
+        }
+        else if (houses == 3)
+        {
+          rent = 800;
+        }
+        else if (houses == 2)
+        {
+          rent = 600;
+        }
+        else if (houses == 1)
+        {
+          rent = 175;
+        }
+        else
+        {
+          rent = 35;
+          if (colorGroupOwner == "DarkBlue")
+          {
+            rent = rent * 2;
+          }
+        }
+      }
+      else if (propertyName == "Baltic Avenue")
+      {
+        if (hotel)
+        {
+          rent = 450;
+        }
+        else if (houses == 4)
+        {
+          rent = 250;
+        }
+        else if (houses == 3)
+        {
+          rent = 100;
+        }
+        else if (houses == 2)
+        {
+          rent = 60;
+        }
+        else if (houses == 1)
+        {
+          rent = 10;
+        }
+        else
+        {
+          rent = 4;
+          if (colorGroupOwner == "Brown")
+          {
+            rent = rent * 2;
+          }
+        }
+      }
+      else
+      {
+        // Default: unknown property
+        Console.WriteLine("Warning: Rent not configured properly for property: " + propertyName);
+        rent = 0;
+      }
+
+      if (isMortgaged)
+      {
+        rent = 0;
+      }
+
+      return rent;
+    }
+
+    public static int RentForBoardwalk(int houses, bool hotel, string colorGroupOwner)
+    {
+      int rent;
+      if (hotel)
+      {
+        rent = 2000;
+      }
+      else if (houses == 4)
+      {
+        rent = 1700;
+      }
+      else if (houses == 3)
+      {
+        rent = 1100;
+      }
+      else if (houses == 2)
+      {
+        rent = 600;
+      }
+      else if (houses == 1)
+      {
+        rent = 200;
+      }
+      else
+      {
+        rent = 50;
+        if (colorGroupOwner == "DarkBlue")
+        {
+          rent = rent * 2;
+        }
+      }
+
+      return rent;
     }
   }
 }
